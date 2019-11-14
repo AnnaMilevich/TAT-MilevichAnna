af8277e28e40c39c97eae51081db7a5372f051d7
anna.milevich27@gmail.com <anna.milevich27@gmail.com>
2019-11-14 10:57:43 +0300

    check

---
+++ Chess/Cell.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Chess
+{
+   public class Cell
+    {
+        char colum;
+        int row;
+
+        public Cell(char colum, int row)
+        {
+         if (row < 1 || row > 8 || (int)colum < 97 || (int)colum > 104)
+            {
+                throw new OutRowOrColumBoard();
+            }
+        Colum = (char)colum;
+        Row = row;
+        }
+        public int Row { get => row; set => row = value; }
+        public char Colum { get => colum ; set => colum = value; }
+        public string GetCellColor()
+        {
+            if ((Row + (int)Colum) % 2 == 0)
+            {
+                return "black";
+            }
+            else return "white";
+        }
+        public int GetAsNumber()
+        {
+            return (row * 10) + ((int)colum- 96);
+        }
+    }
+}
---
+++ Chess/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Cell cell1 = new Cell('c', 8);
+                Cell cell2 = new Cell('e', 1);
+                Chess chess1 = new Chess(cell1, cell2);
+                Console.WriteLine(chess1.GetLocation());
+                Console.WriteLine("{0}{1} cell color: "+ cell1.GetCellColor(),cell1.Colum,cell1.Row);
+                Console.WriteLine("{0}{1} cell color: " + cell2.GetCellColor(), cell2.Colum, cell2.Row);
+
+                Cell cell3 = new Cell('a', 5);
+                Cell cell4 = new Cell('a', 5);
+                Chess chess2 = new Chess(cell3, cell4);
+                Console.WriteLine(chess2.GetLocation());
+                Console.WriteLine("{0}{1} cell color: " + cell3.GetCellColor(), cell4.Colum, cell1.Row);
+                Console.WriteLine("{0}{1} cell color: " + cell3.GetCellColor(), cell4.Colum, cell2.Row);
+            }
+            catch (OutRowOrColumBoard ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
