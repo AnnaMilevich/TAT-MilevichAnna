293a08958c565a07dc06bc8c86515578ea348981
anna.milevich27@gmail.com <anna.milevich27@gmail.com>
2019-11-21 17:45:07 +0300

    Add tests

---
+++ DEV-3Tests/DEV-3Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DEV_3;
+
+namespace DEV_3Tests
+{
+    [TestClass]
+    public class StringTranslatorTester
+    {
+
+        private StringTranslator stringTranslator = new StringTranslator(StringCreator.CreateFromConsole().ToLower());
+
+        [TestMethod]
+        public void TestChecksForStringEmpty()
+        {
+            Assert.ThrowsException<DEV_3.InvalidCharInputException>(() => string.Empty);
+        }
+
+        [TestMethod]
+        public void TestCheckForForbiddenNumders()
+        {
+            Assert.ThrowsException<DEV_3.InvalidCharInputException>(() => "fhsdj4jv");
+        }
+
+        [TestMethod]
+        public void TestCheckForForbiddenChars()
+        {
+            Assert.ThrowsException<DEV_3.InvalidCharInputException>(() => "fhsdj.jv");
+        
+        }
+    }
+}
+
