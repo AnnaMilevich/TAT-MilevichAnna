97e80e1c03c566823622156b2a08b1282edf5f73
anna.milevich27@gmail.com <anna.milevich27@gmail.com>
2019-11-24 18:01:12 +0300

    Add project

---
+++ DEV-4/Page Object/MailLoginPage.cs
@@ -0,0 +1,87 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Linq;
+
+namespace DEV_4
+{
+    /// <summary>
+    /// Class for login page
+    /// </summary>
+    public class MailLoginPage
+    {
+        IWebDriver Driver { get; }
+        WebDriverWait Wait { get; }
+        IWebElement LoginButton { get; set; }
+        IWebElement PassswordButton { get; set; }
+        MailLoginPageLocators Locator { get; }
+
+        /// <summary>
+        /// Class constuctor initializes fields
+        /// </summary>
+        /// <param name="driver"></param>
+        public MailLoginPage(IWebDriver driver)
+        {
+            this.Driver = driver;
+            this.Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+            this.Locator = new MailLoginPageLocators();
+        }
+        /// <summary>
+        /// Goes to login page
+        /// </summary>
+        public void GoToPage()
+        {
+            Driver.Navigate().GoToUrl(Locator.PageLocator);
+        }
+        /// <summary>
+        /// Fills username field on login page
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+
+        public MailLoginPage InputUsername(string username)
+        {
+            Wait.Until(t => Driver.FindElements(By.XPath(Locator.LoginLocator)).Any());
+            Driver.FindElement(By.XPath(Locator.LoginLocator)).SendKeys(username);
+            return this;
+        }
+        /// <summary>
+        /// Fills password field on login page
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+
+        public MailMainPage InputPassword(String password)
+        {
+            Wait.Until(t => Driver.FindElements(By.XPath(Locator.PasswordLocator)).Any());
+            Driver.FindElement(By.Id("mailbox:password")).SendKeys(password+Keys.Enter);
+
+            return new MailMainPage(Driver);
+        }
+        /// <summary>
+        /// Clicks on submit button
+        /// </summary>
+        /// <returns></returns>
+        public MailLoginPage SubmitLogin()
+        {
+            Wait.Until(t => Driver.FindElements(By.XPath(Locator.LoginButtonLocator)).Any());
+            LoginButton = Driver.FindElement(By.XPath(Locator.LoginButtonLocator));
+            LoginButton.Click();
+
+            return this;
+        }
+        /// <summary>
+        /// Logins into account
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public MailMainPage LoginToAccount(string username, string password)
+        {
+            InputUsername(username);
+            SubmitLogin();
+
+            return InputPassword(password);
+        }
+    }
+}
---
+++ DEV-4/Page Object/MailMainPage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DEV_4
+{
+    /// <summary>
+    /// Class for main page.
+    /// </summary>
+    public class MailMainPage
+    {
+
+        IWebDriver Driver { get; }
+        WebDriverWait Wait { get; }
+        IWebElement ReadLetterButton { get; set; }
+        MailMainPageLocators Locator { get; }
+
+        /// <summary>
+        /// Class constuctor initializes fields
+        /// </summary>
+        /// <param name="driver"></param>
+        public MailMainPage(IWebDriver driver)
+        {
+            this.Driver = driver;
+            this.Wait = new WebDriverWait(Driver, TimeSpan.FromMinutes(1));
+            this.Locator = new MailMainPageLocators();
+        }
+
+        /// <summary>
+        /// Counts the number of unread letters
+        /// </summary>
+        public void  MailNumberOfUnreadLetters()
+        {
+            Wait.Until(t => Driver.FindElements(By.XPath(Locator.NumberOfUnreadLettersLocator)).Any());
+            var numberOfUnreadLetters = Driver.FindElement(By.XPath(Locator.NumberOfUnreadLettersLocator)).Text;
+            Console.WriteLine("Number of unread letters: "+numberOfUnreadLetters);
+        }
+
+        /// <summary>
+        /// reads one letters
+        /// </summary>
+        /// <returns></returns>
+        public MailMainPage ClickReadLetterButton()
+        {
+            Wait.Until(t => Driver.FindElements(By.CssSelector("#app-canvas > div > div.application-mail > div.application-mail__overlay > div > div.layout.layout_size_s.layout_type_2pane.layout_left-size_16.layout_left-shortcut.layout_right-size_40.layout_bordered > div.layout__main-frame > div > div > div > div > div > div.dataset-letters > div > div > div > a:nth-child(2) > div.llc__read-status > span")).Any());
+            
+            ReadLetterButton = Driver.FindElement(By.CssSelector("#app-canvas > div > div.application-mail > div.application-mail__overlay > div > div.layout.layout_size_s.layout_type_2pane.layout_left-size_16.layout_left-shortcut.layout_right-size_40.layout_bordered > div.layout__main-frame > div > div > div > div > div > div.dataset-letters > div > div > div > a:nth-child(2) > div.llc__read-status > span"));
+            ReadLetterButton.Click();
+
+            return this;
+        }
+    }
+}
