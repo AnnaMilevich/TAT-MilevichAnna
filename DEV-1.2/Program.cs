a847323372c9711e20a98c6eaa40c4256076bb50
anna.milevich27@gmail.com <anna.milevich27@gmail.com>
2020-02-07 21:55:32 +0300

    add project

---
+++ DEV-1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DEV_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            converter.ConsoleOutput();
+        }
+    }
+}
